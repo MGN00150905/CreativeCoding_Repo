5168d61ff31f70f81dd2beb48ba7e31373e303a4
Marc <marcgallagher16@gmail.com>
2018-02-01 15:41:13 +0000

    file update

--- 01_introduction/04_Proof_of_Probability & Non Uniform Distributions/sketch04.js
+++ 01_introduction/04_Proof_of_Probability & Non Uniform Distributions/sketch04.js
@@ -2,25 +2,40 @@
 // The Nature of Code
 // Daniel Shiffman
 // http://natureofcode.com
+var walker;
 
-funciton setup() {
-  createCanvas(640, 360);
-  background(127);
+function setup() {
+    createCanvas(320, 640);
+    background(127);
+    walker = new Walker();
 }
 
-function draw(){
-  var xloc = randomGaussian();
-  console.log(xloc);
-
-  var sd = 60;
-  var mean = width / 2;
-  xloc = (xloc * sc) + mean;
-
-  fill(0,10);
-  noStroke();
-  ellipse(xloc, height / 2, 16, 16);
-<<<<<<< HEAD
+function draw() {
+    walker.render();
+    walker.step();
 }
-=======
+
+function Walker() {
+  this.x = width/2;
+  this.y = height/2;
+
+  this.render = function() {
+    stroke(0);
+    point(this.x, this.y);
+  };
+  this.step = function() {
+    var probArray = [];
+    probArray[1] = 1;
+    probArray[2] = 2;
+    probArray[3] = 3;
+    probArray[4] = 3;
+    probArray[5] = 3;
+
+    var index = floor(random(probArray.length));
+    var r = probArray[index];
+
+    this.x = constrain(this.x, 0, width - 1);
+    this.y = constrain(this.y, 0, width - 1);
+    
+  };
 }
->>>>>>> 35309b70fafaa9ab1f315e60145c884aa836a1aa
