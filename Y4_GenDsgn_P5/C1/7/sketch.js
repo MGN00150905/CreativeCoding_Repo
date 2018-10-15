ca4a74f11f5ed0ea5c2bb9d55705a074be3fbd7e
Marc <marcgallagher16@gmail.com>
2018-10-15 14:28:50 +0100

    7

--- Y4_GenDsgn_P5/C1/7/sketch.js
+++ Y4_GenDsgn_P5/C1/7/sketch.js
@@ -1,17 +1,37 @@
 //Puts it in strict mode, so for example you cannot use undefined variables
-use 'strict';
+'use strict';
 
-var colourCount = [];
+var colourCount = 20;
 
 //Colour values
 var hueValue = [];
-var saturationValues = [];
-var brightness = [];
+var saturationValue = [];
+var brightnessValue = [];
+var actRandomSeed = 0;
 
 function setup(){
-
+  //Create a canvas the size of the screen
+  //Use HSB colors instead of RGB
+  createCanvas(windowWidth, windowHeight);
+  colorMode(HSB, 360, 100, 100, 100);
+  noStroke();
 }
 
 function draw(){
+  noLoop();
+  randomSeed(actRandomSeed);
 
+  //create palette of colours
+  for(var i = 0; i < colourCount; i++){
+    if (i % 2 == 0) {
+      hueValue[i] = random(130, 220);
+      saturationValue[i] = 100;
+      brightnessValue[i] = random(15, 100);
+    } else {
+      hueValue[i] = 195;
+      saturationValue[i] = random(20, 100);
+      brightnessValue[i] = 100;
+      console.log(hueValue);
+    }
+  }
 }
