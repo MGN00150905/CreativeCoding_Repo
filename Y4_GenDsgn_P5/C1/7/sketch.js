73337cd3d4cc9e30fec7c81aa2fcdff7c8450540
Marc <marcgallagher16@gmail.com>
2018-10-15 12:10:46 +0100

    new structure

---
+++ Y4_GenDsgn_P5/C1/7/sketch.js
@@ -0,0 +1,17 @@
+//Puts it in strict mode, so for example you cannot use undefined variables
+use 'strict';
+
+var colourCount = [];
+
+//Colour values
+var hueValue = [];
+var saturationValues = [];
+var brightness = [];
+
+function setup(){
+
+}
+
+function draw(){
+
+}
